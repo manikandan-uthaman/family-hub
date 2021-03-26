f95b3997a1a587a665bc49707e023abbf349e439
Manikandan Uthaman <manikandan.mit@outlook.com>
2021-03-26 16:15:45 +0530

    FH-7: Registration functionality implemented

--- src/services/authService.js
+++ src/services/authService.js
@@ -12,6 +12,12 @@ async function login(username, password) {
   return response.token;
 }
 
+async function register(data) {
+  const response = await httpService.post('/register', data);
+  localStorage.setItem(tokenKey, response.token);
+  return response.token;
+}
+
 function getCurrentUser() {
   const token = localStorage.getItem(tokenKey);
   if (!token) return null;
@@ -29,6 +35,7 @@ function logout() {
 const auth = {
   login,
   logout,
+  register,
   getCurrentUser,
 };
 
