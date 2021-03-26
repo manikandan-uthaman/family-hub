4c158d214818e18629b281c04263260e0c4e9a7d
Manikandan Uthaman <manikandan.mit@outlook.com>
2021-03-26 15:32:43 +0530

    Login logout functionalities implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import './App.scss';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/nav-bar/navBar';
 import Home from './components/home/home';
-import { Route, Switch } from 'react-router-dom';
+import Login from './components/login/login';
+import auth from './services/authService';
+import './App.scss';
+import Logout from './components/Logout/logout';
 
-function App() {
-  return (
-    <div className="app">
-      <NavBar></NavBar>
-      <div className="content">
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div className="app">
+        <NavBar user={user}></NavBar>
+        <div className="content">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+            <Route
+              path="/"
+              render={(props) => <Home {...props} user={user} />}
+            />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
